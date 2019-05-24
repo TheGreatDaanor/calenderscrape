bea81afdcb78aa1eeebca16f7c5b0a0fb9a62dc9
TheGreatDaanor <47156297+TheGreatDaanor@users.noreply.github.com>
2019-05-24 19:56:13 +0200

    Add files via upload

---
+++ autologin.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+
+url='https://app.planning.nu/madurodam/madurodam-b.v./'
+page = requests.get(url)
+soup = BeautifulSoup(page.text,"html.parser")
+#print(soup.prettify())
+
+payload = {
+    'xvalues[user]': 'dangelo',
+    'xvalues[pass]': '',
+    'submit':'login',
+    'action':'submit'
+}
+
+with requests.session() as s:
+    s.get(url)
+    p = s.post('https://app.planning.nu/madurodam/madurodam-b.v./', data=payload)
+#   print(p.text)
+
+    r = s.get('https://app.planning.nu/madurodam/madurodam-b.v./rooster2/index2')
+    print (r.text)
+
+data = requests.get("https://app.planning.nu/madurodam/madurodam-b.v./rooster2/index2")
+
+
+
+
