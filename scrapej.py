1ca0430e5b466d2f723465546209b4401f01dfbd
TheGreatDaanor <47156297+TheGreatDaanor@users.noreply.github.com>
2019-07-22 20:35:46 +0200

    Add files via upload

---
+++ scrapej.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import requests
+import time
+
+url='https://app.planning.nu/madurodam/madurodam-b.v./'
+page = requests.get(url)
+
+
+soup = BeautifulSoup(page.text,"html.parser")
+#print(soup.prettify())
+
+payload = {
+    'xvalues[user]': 'dangelo',
+    'xvalues[pass]': 'r46dCp$lWh',
+    'submit':'login',
+    'action':'submit'
+}
+
+with requests.session() as s:
+    s.get(url)
+    p = s.post('https://app.planning.nu/madurodam/madurodam-b.v./', data=payload)
+#   print(p.text)
+
+    r = s.get('https://app.planning.nu/madurodam/madurodam-b.v./rooster2/index2')
+    print(r.text)
